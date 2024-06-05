fcb88951cd8b85ceedf765c7ea0ad38dfa8f9ab3
Bernardo Raposo <daviraposo124@gmail.com>
2024-06-05 01:04:27 +0000

    style: estilização do h1 e do p do card

--- src/Pages/Home.styles.js
+++ src/Pages/Home.styles.js
@@ -4,7 +4,22 @@ export const CardContainer = styled.div`
     background: ${props => props.theme.cardGradient};
     max-width: 41.2rem;
     padding: 3.2rem;
-    
+    border-radius: 3rem;
+
+
+    h1 {
+        font-size: 2.8rem;
+        color: ${props => props.theme.white};
+        margin-bottom: 1.5rem;
+        text-align: center;;
+    }
+
+    p {
+        font-size: 1.5rem;
+        line-height: 2.4rem;        
+        color: ${props => props.theme.textColor};
+        text-align: center;
+    }
 `
 
 export const IconContainer = styled.div``
