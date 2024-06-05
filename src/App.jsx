d00b04e4c3c103e497723e3d5572cb90fc3bfb47
Bernardo Raposo <daviraposo124@gmail.com>
2024-06-05 00:23:30 +0000

    feat: instalação e implantação do react-router-dom e do styled-components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
-export default function App() {
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import { ThemeProvider } from "styled-components"
+
+
+import { Home } from "./Pages/Home"
+import { theme } from "./styles/theme"
+
+const router = createBrowserRouter ([
+
+  {
+    path: "/",
+    element: <Home />
+  }
+])
+
+export function App() {
   return (
-    <div>App</div>
+    <ThemeProvider theme={theme}>
+          <RouterProvider router={router} />
+    </ThemeProvider>
   )
 }
---
+++ src/styles/theme.js
@@ -0,0 +1,9 @@
+export const theme = {
+    homeBackground: "#131518",
+    cardGradient: "radial.gradient(98.96% 98.96% at 50% 0%, #232A34 0%, #181E27 100%)",
+    textColor: "#969FAD",
+    submitButtonColor: "#FC7614",
+    white: "#FFF",
+    feedbackButtonColor: "#262E38",
+    feedbackButtonText: "#7C8798"
+}
